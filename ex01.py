c64ce5ce3dc242e3d73cf82ba066a04ada8d5451
Michel <106674127+MichelZero@users.noreply.github.com>
2025-06-09 07:51:13 -0300

    up

---
+++ ex01.py
@@ -0,0 +1,15 @@
+# autor: Michel
+# data: 09/06/2025
+#
+
+KB = 1024
+MB = KB * 1024
+GB = MB * 1024
+TB = GB * 1024
+PB = TB * 1024
+
+print(f"1 KB = {KB} byte")
+print(f"1 MB = {MB} byte")
+print(f"1 GB = {GB} byte")
+print(f"1 TB = {TB} byte")
+print(f"1 PB = {PB} byte")
